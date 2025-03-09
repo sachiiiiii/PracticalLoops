4144dde1d30aa5b74b0a9e20f60a9cc2ca4b7791
Osi <osachi752@gmail.com>
2025-03-09 19:50:54 -0400

    part2: prime time

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
+// /**
+//  * Part 1: Fizz Buzz
+//  */
+// console.log(`
+// Part 1: Fizz Buzz`)
+
+// // Loop through all numbers from 1 to 100.
+// // If a number is divisible by 3, log “Fizz.”
+// // If a number is divisible by 5, log “Buzz.”
+// // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// // If a number is not divisible by either 3 or 5, log the number.
+// for (num = 1; num <= 100; num++) {
+//     switch (true) {
+//         case (num % 3 === 0 && num % 5 === 0):
+//             console.log("Fizz Buzz");
+//             break;
+
+//         case (num % 3 === 0):
+//             console.log("Fizz");
+//             break;
+
+//         case (num % 5 === 0):
+//             console.log("Buzz");
+//             break;
+
+//         default:
+//             console.log(num)
+//             break;
+//     }
+// }
+
 /**
- * Part 1: Fizz Buzz
+ * Part 2: Prime Time
  */
 console.log(`
-Part 1: Fizz Buzz`)
+Part 2: Prime Time`)
 
-for (num = 1; num <= 100; num++) {
-    // if (num % 3 === 0 && num % 5 === 0) {
-    //     console.log("Fizz Buzz")
-    // }
-    switch (true) {
-        case (num % 3 === 0 && num % 5 === 0):
-            console.log("Fizz Buzz");
-            break;
+const n = 829;
+let divisibleBySelf = true;
+let divisibleByOne = true;
+let OtherMultiples = 0;
 
-        case (num % 3 === 0):
-            console.log("Fizz");
-            break;
-
-        case (num % 5 === 0):
-            console.log("Buzz");
-            break;
-
-        default:
-            console.log(num)
-            break;
+let nextPrimeNum = n + 1;
+while (n > 1) {
+    for (i = n; i > 1; i--) {
+        if (nextPrimeNum % i === 0) {
+            OtherMultiples++;
+        }
+    }
+    if (divisibleByOne && divisibleBySelf && OtherMultiples === 0) {
+        console.log(nextPrimeNum);
+        break;
+    } else {
+        nextPrimeNum++;
+        OtherMultiples = 0;
     }
 }
 
+// for (i = n; i > 1; i--) {
+//     if (nextPrimeNum % i === 0) {
+//         OtherMultiples++;
+//     }
+// }
+// if (divisibleByOne && divisibleBySelf && OtherMultiples === 0) {
+//     console.log(nextPrimeNum);
+//     break;
+// } else {
+//     nextPrimeNum++;
+//     OtherMultiples = 0;
+// }
